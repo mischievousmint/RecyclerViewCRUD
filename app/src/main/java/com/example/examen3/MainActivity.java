579ef9b2058522734fa00c4f26122883be6e57dd
Lory <mentamaliciosa@gmail.com>
2019-06-25 20:55:35 +0200

    version3 esperando el dialog

--- app/src/main/java/com/example/examen3/MainActivity.java
+++ app/src/main/java/com/example/examen3/MainActivity.java
@@ -1,23 +1,35 @@
 package com.example.examen3;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.AbstractQueue;
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.support.v7.widget.helper.ItemTouchHelper.Callback.makeMovementFlags;
+import static com.example.examen3.MySharedPreferences.saveNote;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnAdd, btnMove, btnDel;
+    Button btnAdd, btnMove, btnDel, btnAddNote;
     RecyclerView rvNotes;
-    List<NotesModel> listNotes;
+    static List<NotesModel> listNotes;
     NotesAdapter myAdapter;
+    EditText etTitle, etBody;
+    String newTitle;
+    String newBody;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +41,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnDel = findViewById(R.id.btnDel);
         rvNotes = findViewById(R.id.rvNotes);
 
+
+
         btnAdd.setOnClickListener(this);
         btnMove.setOnClickListener(this);
         btnDel.setOnClickListener(this);
+        //btnAddNote.setOnClickListener(this);
 
 
         listNotes = getNotes();
@@ -42,10 +57,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    public void  addNote(String title, String body) {
+/*    public void openDialog() {
+        final Dialog dialog = new Dialog(getApplicationContext()); // Context, this, etc.
+        dialog.setContentView(R.layout.dialog_note);
+        newTitle = etTitle.getText().toString();
+        newBody = etBody.getText().toString();
+        // dialog.setTitle(R.string.dialog_title);
+        dialog.show();
+    }*/
+
+
+    public AlertDialog createAddDialogo() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+        LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+
+        View v = inflater.inflate(R.layout.dialog_note,null);
+
+
+        etTitle = v.findViewById(R.id.etTitle);
+        etBody = v.findViewById(R.id.etBody);
+        btnAddNote = v.findViewById(R.id.btnAddNote);
+
+        builder.setView(v);
+/*
+        newTitle = etTitle.getText().toString();
+        newBody = etBody.getText().toString();
+
+        btnAddNote.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                       saveNote(listNotes);
+                    }
+                }
+        );*/
+
+        return builder.create();
+    }
+
+
+
+    public void  addNote(String newTitle, String newBody) {
         NotesModel newNote = new NotesModel();
-        newNote.setTitle(title);
-        newNote.setBody(body);
+        createAddDialogo();
+        //openDialog();
+        newNote.setTitle(newTitle);
+        newNote.setBody(newBody);
         listNotes.add(newNote);
         myAdapter.notifyItemInserted(listNotes.size());
     }
@@ -55,12 +113,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         myAdapter.notifyItemRemoved(listNotes.size());
     }
 
+/*
+    public void moveNote() {
+        listNotes.swap;
+        notifyItemMoved();
+    }
+*/
     public void onClick(View v) {
     int aver = v.getId();
         switch (aver) {
             case R.id.btnAdd:
                 addNote("new note", "new body" );
                 Toast.makeText(getApplicationContext(),"Hello I am btnAdd", Toast.LENGTH_LONG).show();
+                createAddDialogo();
                 break;
             case R.id.btnMove:
                 Toast.makeText(getApplicationContext(),"Hello I am btnMove", Toast.LENGTH_LONG).show();
---
+++ app/src/main/java/com/example/examen3/MySharedPreferences.java
@@ -0,0 +1,47 @@
+package com.example.examen3;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class MySharedPreferences {
+
+    private  static SharedPreferences mySharedPref;
+    static final String KEYNAME = "note";
+    private static Object listNotes;
+
+    private MySharedPreferences() {
+
+    }
+
+    public static void  init (Context context) {
+        if(mySharedPref == null) {
+            mySharedPref = context.getSharedPreferences("MYPREFS", Activity.MODE_PRIVATE); //MYPREFS es el nombre del a BBDD
+        }
+    }
+
+    public static void  saveNote(List<NotesModel> listNotes) {
+        SharedPreferences.Editor prefsEditor = mySharedPref.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(listNotes);
+        prefsEditor.putString(KEYNAME, json);
+        prefsEditor.apply();
+    }
+
+    public static List<NotesModel> loadNote() {
+        Gson gson = new Gson();
+        String retrieveNote = mySharedPref.getString("MYNOTES", "");
+        Type typeNote = new TypeToken<List<NotesModel>>(){}.getType();
+        List<NotesModel> listNotes = gson.fromJson(retrieveNote, typeNote);
+
+        return listNotes;
+
+    }
+}
